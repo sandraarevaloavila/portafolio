670e0908a1953920c9c7a241713f55da4c6dd7d2
sandraarevaloavila <sandrita.bibi12@gmail.com>
2022-11-29 14:54:47 -0500

    sprint4

--- portafolio_backend/contac/Api/urls.py
+++ portafolio_backend/contac/Api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from portafolio_backend.contac.Api.api import contact_api_view
 
 urlpatterns = [
-    path('contact/', contact_api_view, name='contact')
+    path('contac/', contact_api_view, name='contac')
 ]
--- portafolio_backend/urls.py
+++ portafolio_backend/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contact/', include('portafolio_backend.contact.api.urls')),
+    path('contac/', include('portafolio_backend.contac.api.urls')),
 ]
